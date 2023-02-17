ee11b18c0edcd0dc42584a44e6e20d6b58620337
piyush <piyushsharmashpr@gmail.com>
2023-02-17 19:00:57 +0530

    [styles] made changes to body intro

--- src/Components/Body/BodyIntro.jsx
+++ src/Components/Body/BodyIntro.jsx
@@ -5,7 +5,6 @@ export const BodyIntro = () => {
         <div className="intro-section-1">
             <h1 className="intro-header">Federation of Consumer Association Sikkim</h1>
             <div className="intro-description">The organization, established in 1997, is a non-profit, non-governmental, and voluntary association.</div>
-
             <Button variant="warning" className='navbar-cta-button intro-body-cta-button'>FCAS</Button>
         </div>
         <div className="image-container">
