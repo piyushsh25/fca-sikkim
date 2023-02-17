ec7920240552150c532e115bfaac22dcf766813e
piyush <piyushsharmashpr@gmail.com>
2023-02-17 20:06:46 +0530

    removed google plus icon

--- src/Components/Body/Footer.jsx
+++ src/Components/Body/Footer.jsx
@@ -12,7 +12,6 @@ export const Footer = () => {
                 <a href="#"><i class="fa fa-facebook"></i></a>
                 <a href="#"><i class="fa fa-twitter"></i></a>
                 <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-google-plus"></i></a>
                 <a href="#"><i class="fa fa-instagram"></i></a>
             </div>
         </div>
