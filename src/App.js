6b9c9bad8d90b38cf2d36fafa9fab2ea812e357d
piyush <piyushsharmashpr@gmail.com>
2023-02-18 11:49:48 +0530

    about us page complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Homepage } from './Pages/Homepage';
 import { About } from './Pages/About';
+import { ContactPage } from './Pages/Contact';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
+          <Route path='/contact-us' element={<ContactPage />} />
         </Routes>
       </BrowserRouter>
     </div>
