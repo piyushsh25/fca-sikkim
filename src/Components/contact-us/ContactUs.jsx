ff7206bdac5accbcc2598d173190ad53ad4562f6
rinchentb <rinchentb26@gmail.com>
2023-02-18 13:27:45 +0530

    changes

--- src/Components/contact-us/ContactUs.jsx
+++ src/Components/contact-us/ContactUs.jsx
@@ -1,102 +1,94 @@
 import "./ContactUs.css";
 export const ContactUs = () => {
-    return <section>
-        <h1 className="contact-use-header">Contact FCA Helpline:</h1>
-        <div class="contact-cards container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Mr Suresh Kumar Lama (President)
-                            </span>
-                            <p class="text">
-                                Phone : 97756-95114
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-map-o"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Our Head Office                        </span>
-                            <p class="text">
-                                Mist Tree Mountain , Hospital Dara
-                                Gangtok - Sikkim - India
-                                Pin - 737101
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-phone"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Call FCA
-                            </span>
-                            <p class="text">
-                                Phone : (+91) 03592 - 295033 <br />
-                                Mobile: +386 5555 235 / +386 8754 231
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-envelope-o"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Mail FCA
-                            </span>
-                            <p class="text">
-                                <a href="mailto:federationconsumersikkim1997@gmail.com">federationconsumersikkim1997@gmail.com</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <h1 className="contact-use-header">Contact State Helpline:</h1>
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-map-o"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                State Helpline Number                        </span>
-                            <p class="text">
-                                Phone : 03592-202675
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-map-o"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                State Complaint Registry                   </span>
-                            <p class="text">
-                                <a href="https://edaakhil.nic.in">Click to Register Online Complaint</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section>
+      <h1 className="contact-use-header">Contact FCA Helpline:</h1>
+      <div class="contact-cards container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">Mr Suresh Kumar Lama (President)</span>
+                <p class="text">Phone : 97756-95114</p>
+              </div>
             </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-map-o"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">Our Head Office </span>
+                <p class="text">
+                  Mist Tree Mountain , Hospital Dara <br></br>Gangtok - Sikkim -
+                  India <br></br>Pin - 737101
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-phone"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">Call FCA</span>
+                <p class="text">
+                  Landline : (+91) 03592 - 295033 <br />
+                  Mobile: (+91) 97756 95114<br></br>Mobile: (+91) 90026 27682
+                  <br></br>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-envelope-o"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">Mail FCA</span>
+                <p class="text">
+                  <a href="mailto:federationconsumersikkim1997@gmail.com">
+                    federationconsumersikkim1997@gmail.com
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <h1 className="contact-use-header">Contact State Helpline:</h1>
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-map-o"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">State Helpline Number </span>
+                <p class="text">Phone : 03592-202675</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-map-o"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">State Complaint Registry </span>
+                <p class="text">
+                  <a href="https://edaakhil.nic.in">
+                    Click to Register Online Complaint
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-
+      </div>
     </section>
+  );
 };
