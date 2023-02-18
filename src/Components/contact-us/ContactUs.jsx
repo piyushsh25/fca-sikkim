c9d48fd4dbd0afab5c7ffbae732a07fb06cbed24
rinchentb <rinchentb26@gmail.com>
2023-02-18 12:47:46 +0530

    changes

--- src/Components/contact-us/ContactUs.jsx
+++ src/Components/contact-us/ContactUs.jsx
@@ -1,57 +1,52 @@
-import "./ContactUs.css"
+import "./ContactUs.css";
 export const ContactUs = () => {
-    return <section>
-        <div class="contact-cards container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-phone"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Call us anytime
-                            </span>
-                            <p class="text">
-                                Phone : (+91) 03592 - 295033
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-map-o"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Our Head Office                        </span>
-                            <p class="text">
-                                Mist Tree Mountain , Hospital Dara
-                                Gangtok - Sikkim - India
-                                Pin - 737101
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="card">
-                        <span class="icon">
-                            <i class="fa fa-envelope-o"></i>
-                        </span>
-                        <div class="content-wrap">
-                            <span class="item-title">
-                                Call us anytime
-                            </span>
-                            <p class="text">
-                                Mobile: +386 5555 235 / +386 8754 231
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section>
+      <div class="contact-cards container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-phone"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">Call us anytime</span>
+                <p class="text">
+                  Phone : (+91) 03592 - 295033 <br></br>
+                  Mobile: (+91) 9775695114 <br></br>
+                  Mobile: (+91) 9002627682
+                </p>
+              </div>
             </div>
-        </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-map-o"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title">Head Office </span>
+                <p class="text">
+                  Mist Tree Mountain, Hospital Dara<br></br> Gangtok - Sikkim -
+                  India <br></br>Pin - 737101
+                </p>
+              </div>
+            </div>
+          </div>
 
+          <div class="col-md-4">
+            <div class="card">
+              <span class="icon">
+                <i class="fa fa-envelope-o"></i>
+              </span>
+              <div class="content-wrap">
+                <span class="item-title"> Email Us</span>
+                <p class="text">federationconsumersikkim1997@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-}
+  );
+};
