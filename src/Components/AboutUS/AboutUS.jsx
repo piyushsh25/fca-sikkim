a4ff3fd21dc4cdb260e7ea24957e9e4411a4e3c0
rinchentb <rinchentb26@gmail.com>
2023-02-25 22:35:04 +0530

    about us

--- src/Components/AboutUS/AboutUS.jsx
+++ src/Components/AboutUS/AboutUS.jsx
@@ -12,15 +12,20 @@ export const AboutUS = () => {
         of Sikkim. FCAS is instrumental on promoting Consumer Rights in the
         state till date, besides protects consumer interests particularly to
         concretise the consumer rights and remedies enacted under the Consumer
-        Protection Act . FCAS is the member of State Consumer Protection
-        Council. Registration No. 990/Vol.I/1990 NGO Darpan Unique ID No.
-        SK/2022/0318444.
+        Protection Act . <br />
+        <br />
+        FCAS is the member of State Consumer Protection Council. <br />
+        Registration No. 990/Vol.I/1990 <br />
+        NGO Darpan Unique ID No. SK/2022/0318444.
       </div>
       <div className="about-us-content-2">
-        President Mr. Suresh Kumar Lama Vice President Mrs. Srijana Kadka Mr.
-        Kesang Tamang General Secretary Mr. Amartya Raj Gurung Treasure Mr.
-        Gaurav Kumar Chief Coordinator (State) Miss Rinzing Doma Tamang
-        Coordinator Mr. Ashal Chettri Publicity Secretary Mr. Nitesh R. Pradhan
+        President --> Mr. Suresh Kumar Lama <br />
+        Vice Presidents --> Mrs. Srijana Kadka & Mr. Kesang Tamang <br />
+        General Secretary --> Mr. Amartya Raj Gurung <br />
+        Treasure --> Mr. Gaurav Kumar <br /> Chief Coordinator (State)--> Miss
+        Rinzing Doma Tamang <br />
+        Coordinator -->Mr. Ashal Chettri <br />
+        Publicity Secretary --> Mr. Nitesh R. Pradhan <br />
       </div>
     </div>
   );
