4bfbf9fca5d20ecf89ddee112b848228d0fc2648
rinchentb <rinchentb26@gmail.com>
2023-02-25 22:28:34 +0530

    abotus

--- src/Components/AboutUS/AboutUS.jsx
+++ src/Components/AboutUS/AboutUS.jsx
@@ -7,46 +7,20 @@ export const AboutUS = () => {
         <img src={require("../assets/1.jpg")} alt="About Us" />
       </div>
       <div className="about-us-content-1">
-        There are many variations of passages of Lorem Ipsum available, but the
-        majority have suffered alteration in some form, by injected humour, or
-        randomised words which don't look even slightly believable. If you are
-        going to use a passage of Lorem Ipsum, you need to be sure there isn't
-        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
-        generators on the Internet tend to repeat predefined chunks as
-        necessary, making this the first true generator on the Internet. It uses
-        a dictionary of over 200 Latin words, combined with a handful of model
-        sentence structures, to generate Lorem Ipsum which looks reasonable. The
-        generated Lorem Ipsum is therefore always free from repetition, injected
-        humour, or non-characteristic words etc. It is a long established fact
-        that a reader will be distracted by the readable content of a page when
-        looking at its layout. The point of using Lorem Ipsum is that it has a
-        more-or-less normal distribution of letters, as opposed to using
-        'Content here, content here', making it look like readable English. Many
-        desktop publishing packages and web page editors now use Lorem Ipsum as
-        their default model text, and a search for 'lorem ipsum' will uncover
-        many web sites still in their infancy. Various versions have evolved
-        over the years, sometimes by accident, sometimes on purpose (injected
-        humour and the like).
+        Federation of Consumers Association of Sikkim is formed in the year
+        1997. Since then FCAS is the only Voluntary Consumer Organisation (VCO)
+        of Sikkim. FCAS is instrumental on promoting Consumer Rights in the
+        state till date, besides protects consumer interests particularly to
+        concretise the consumer rights and remedies enacted under the Consumer
+        Protection Act . FCAS is the member of State Consumer Protection
+        Council. Registration No. 990/Vol.I/1990 NGO Darpan Unique ID No.
+        SK/2022/0318444.
       </div>
       <div className="about-us-content-2">
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum."
+        President Mr. Suresh Kumar Lama Vice President Mrs. Srijana Kadka Mr.
+        Kesang Tamang General Secretary Mr. Amartya Raj Gurung Treasure Mr.
+        Gaurav Kumar Chief Coordinator (State) Miss Rinzing Doma Tamang
+        Coordinator Mr. Ashal Chettri Publicity Secretary Mr. Nitesh R. Pradhan
       </div>
     </div>
   );
